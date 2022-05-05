0a4b7da7d1264009016a50d20bf6ee0cae69f0ee
aluno <mugrillo1456@gmail.com>
2022-05-04 21:18:57 -0300

    ajustes

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Scanner;
 import entities.Aluno;
 import jdbc.AlunoJDBC;
@@ -17,26 +19,22 @@ public class Programa {
 
 		Connection con = DB.getConexao();
 		System.out.println("Conexão realizada com sucesso !");
-		
-		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy"); 
-		
+
+		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
 		Scanner console = new Scanner(System.in);
-		
+
 		int opcao = 0;
-		
+
 		try {
 
 			do {
-				System.out.print("######## Menu ########" + 
-								"\n1- Cadastrar" + 
-								"\n2- Listar"    + 
-								"\n3- Alterar"   +
-								"\n4- Excluir"   + 
-								"\n5- Sair"      +
-								"\n\tOpção: ");
+				System.out.print("######## Menu ########" + "\n1- Cadastrar" + "\n2- Listar" + "\n3- Alterar"
+						+ "\n4- Excluir" + "\n5- Sair" + "\n\tOpção: ");
 				opcao = Integer.parseInt(console.nextLine());
 
-				if (opcao == 1) {
+				switch (opcao) {
+				case 1:
 
 					Aluno a = new Aluno();
 					AlunoJDBC acao = new AlunoJDBC();
@@ -46,20 +44,63 @@ public class Programa {
 					a.setNome(console.nextLine());
 					System.out.print("Sexo: ");
 					a.setSexo(console.nextLine());
-		
+
 					System.out.print("Data de nascimento (dd/MM/yyyy): ");
-					a.setDt_nasc( Date.valueOf( LocalDate.parse( console.nextLine(), formato) ) ) ;
-					
+					a.setDt_nasc(Date.valueOf(LocalDate.parse(console.nextLine(), formato)));
+
 					acao.salvar(a, con);
 					console.nextLine();
+					break;
+
+				case 2:
+					System.out.println("Listar aluno");
+					AlunoJDBC listarAluno = new AlunoJDBC();
+					List<Aluno> alunoList = listarAluno.listar(con);
+
+					for (Aluno objectAluno : alunoList) {
+						System.out
+								.println("Aluno: " + objectAluno.getId() + objectAluno.getNome() + objectAluno.getSexo()
+										+ new SimpleDateFormat("dd-MM-yyyy HH").format(objectAluno.getDt_nasc()));
+						console.nextLine();
+					}
+
+					break;
+
+				case 3:
+					Aluno aluno = new Aluno();
+					System.out.println("Insira o ID do aluno");
+					aluno.setId(console.nextInt());
+					console.nextLine();
+					System.out.print("Nome: ");
+					aluno.setNome(console.nextLine());
+					System.out.print("Sexo: ");
+					aluno.setSexo(console.nextLine());
+					System.out.print("Data de nascimento (dd/MM/yyyy): ");
+					aluno.setDt_nasc(Date.valueOf(LocalDate.parse(console.nextLine(), formato)));
+					AlunoJDBC.alterar(aluno, con);
+
+					break;
+
+				case 4:
+
+					AlunoJDBC excluirAlunoJDBC = new AlunoJDBC();
+					System.out.println("Excluir aluno");
+					System.out.println("Insira o ID do aluno que deseja excluir: ");
+					excluirAlunoJDBC.apagar(console.nextInt(), con);
+					console.nextLine();
+					break;
+
+				default:
+					System.out.println("haha");
+					break;
 				}
-				
+
 			} while (opcao != 5);
 
 		} catch (Exception e) {
 			System.out.println("Erro: " + e);
 		}
-		
+
 		DB.fechaConexao();
 		System.out.println("Conexão fechada com sucesso !");
 	}
--- src/jdbc/AlunoJDBC.java
+++ src/jdbc/AlunoJDBC.java
@@ -1,55 +1,96 @@
 package jdbc;
 
 import java.io.IOException;
+import java.net.ConnectException;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import entities.Aluno;
 
 public class AlunoJDBC {
-	
-	String sql;
-	PreparedStatement pst;
-	
-	
-	public void salvar(Aluno a, Connection con) throws IOException {
-		
+
+	static String sql;
+	static PreparedStatement pst;
+
+	public static void salvar(Aluno a, Connection con) throws IOException {
+
 		try {
-			
+
 			sql = "INSERT INTO aluno (nome, sexo, dt_nasc) VALUES (?,  ?, ?)";
-			
+
 			pst = con.prepareStatement(sql);
 			pst.setString(1, a.getNome());
 			pst.setString(2, a.getSexo());
-			
+
 			Date dataSql = new Date(a.getDt_nasc().getTime());
 			pst.setDate(3, dataSql);
-			
+
 			pst.executeUpdate();
 			System.out.println("\nCadastro do aluno realizado com sucesso!");
-			
-		}
-		catch (SQLException e) {
-			
+
+		} catch (SQLException e) {
+
 			System.out.println(e);
 		}
-		
-	}
-	
-	public List<Aluno> listar() {
 
-		return null;
 	}
-	
-	public void apagar(int id) {
-		
+
+	public static List<Aluno> listar(Connection conextionDatabase) throws IOException {
+		List<Aluno> alunoList = new ArrayList();
+
+		try {
+			sql = "SELECT * FROM aluno";
+			pst = conextionDatabase.prepareStatement(sql);
+			ResultSet resultSet = pst.executeQuery();
+			Aluno aluno = new Aluno();
+			while (resultSet.next()) {
+
+				System.out.println(resultSet.getInt(1) + "|" + resultSet.getString(2) + "|" + resultSet.getString(3)
+						+ "|" + resultSet.getDate("dt_Nasc"));
+			}
+
+			alunoList.add(aluno);
+
+		} catch (SQLException error) {
+			System.out.println("Erro de conexão. Tente novamente");
+
+		} finally {
+			System.out.println("finally process");
+		}
+
+		return alunoList;
 	}
-	
-	public void alterar(Aluno a) {
-		
+
+	public static void apagar(int id, Connection conextionDatabase) throws SQLException {
+
+		sql = "DELETE FROM aluno WHERE id = ?";
+		pst = conextionDatabase.prepareStatement(sql);
+		pst.setInt(1, id);
+		if (pst.executeUpdate() >= 1) {
+			System.out.println("Aluno excluido");
+		} else {
+			System.out.println("Aluno nao excluido");
+		}
+	}
+
+	public static void alterar(Aluno a, Connection conextionDatabase) throws SQLException {
+		sql = "UPDATE aluno SET nome = ?, sexo= ?, dt_nasc=? WHERE id = ?";
+		pst = conextionDatabase.prepareStatement(sql);
+		pst.setString(1, a.getNome());
+		pst.setString(2, a.getSexo());
+		pst.setInt(4, a.getId());
+		Date dataSql = new Date(a.getDt_nasc().getTime());
+		pst.setDate(3, dataSql);
+
+		if (pst.executeUpdate() == 1) {
+			System.out.println("Alterado com sucesso");
+		} else {
+			System.out.println("Erro ao cadastrar aluno");
+		}
 	}
 }
-
